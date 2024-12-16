d1829d92451b118e1f89c46a00341f02674624cd
SannidhiBhatRobosoft <sannidhi.bhat@robosoftin.com>
2024-12-16 09:51:41 +0530

    header added to shop

--- src/app/pages/shopping/shopping.component.ts
+++ src/app/pages/shopping/shopping.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { ShopComponent } from "../../components/shop/shop.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-shopping',
   standalone: true,
-  imports: [NavbarComponent, ShopComponent, FooterComponent],
+  imports: [NavbarComponent, ShopComponent, FooterComponent,HeaderComponent],
   templateUrl: './shopping.component.html',
   styleUrl: './shopping.component.css'
 })
